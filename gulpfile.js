6a1bef5b300ba10208a8fc586569b641d6e2c446
Rafael Pedrola <rafinskipg@gmail.com>
2014-02-24 12:16:12 +0100

    added gulp load tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,27 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    minifyHTML = require('gulp-minify-html'),
-    minifyCSS = require('gulp-minify-css'),
-    imagemin = require('gulp-imagemin'),
-    browserify = require('gulp-browserify'),
-    concat = require('gulp-concat'),
-    clean = require('gulp-clean'),
+    tasks = require("gulp-load-tasks")(),
     lr = require('tiny-lr'),
-    watch = require('gulp-watch'),
-    livereload = require('gulp-livereload'),
-    open = require("gulp-open"),
     server = lr(),
     connect = require('connect'), 
     http = require('http'), 
-    sass = require('gulp-sass'),
     runSequence = require('run-sequence'),
     jade = require('gulp-jade');
 
 
 gulp.task('lint', function() {
   return gulp.src('./app/scripts/app.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(tasks.jshint())
+    .pipe(tasks.jshint.reporter('default'));
 });
 
 gulp.task('clean', function() {
    return gulp.src(['./build/', './dist/'], {read: false})
-        .pipe(clean());
+        .pipe(tasks.clean());
 });
 
 gulp.task('browserify', function() {
    return gulp.src(['./app/scripts/app.js'])
-        .pipe(browserify({
+        .pipe(tasks.browserify({
           insertGlobals : true,
           debug : true,
           shim: {
@@ -65,7 +53,7 @@ gulp.task('browserify', function() {
             }
           }
         }))
-        .pipe(concat('main.js'))
+        .pipe(tasks.concat('main.js'))
         .pipe(gulp.dest('./build/scripts'))
 });
 
@@ -81,7 +69,7 @@ gulp.task('templates', function() {
 
 gulp.task('sass', function(){
   return  gulp.src('app/styles/custom.scss')
-        .pipe(sass({                      // Dictionary of render options
+        .pipe(tasks.sass({                      // Dictionary of render options
                 includePaths: [
                     './app/bower_components/bootstrap-sass/lib/',
                     '../bower_components/bootstrap-sass/lib/',
@@ -93,26 +81,26 @@ gulp.task('sass', function(){
 
 gulp.task('minify-css', function() {
   gulp.src('./build/styles/**/**.css')
-    .pipe(minifyCSS(opts))
+    .pipe(tasks['minify-css'](opts))
     .pipe(gulp.dest('./dist/styles'))
 });
 
 gulp.task('minify-html', function() {
   gulp.src('./build/views/*.html')
-    .pipe(minifyHTML(opts))
+    .pipe(tasks['minify-html'](opts))
     .pipe(gulp.dest('./dist/views'))
 });
 
 gulp.task('imagemin', function () {
     gulp.src('app/images/**.{png,svg,jpeg,gif,jpg}')
-        .pipe(imagemin())
+        .pipe(tasks.imagemin())
         .pipe(gulp.dest('dist'));
 });
 
 //TODO add support for source maps
 gulp.task('minify-js', function() {
   gulp.src('./build/scripts/**/*.js')
-    .pipe(uglify({
+    .pipe(tasks.uglify({
             // inSourceMap: 
             // outSourceMap: "app.js.map"
     }))
@@ -121,7 +109,7 @@ gulp.task('minify-js', function() {
 
 gulp.task('watch', function() {
     return gulp.src(['./app/scripts/**/**.**','./app/styles/**.**','./app/views/**.**'])
-        .pipe(watch({ emit: 'all' }, function(files) {
+        .pipe(tasks.watch({ emit: 'all' }, function(files) {
             files
                 .pipe(gulp.run('templates','sass', 'lint', 'browserify'));
         }));
@@ -141,7 +129,7 @@ gulp.task("server", function(){
     gulp.run('watch');
 
     return gulp.src("./app/index.html")
-        .pipe(open("", options));
+        .pipe(tasks.open("", options));
 });
 
 gulp.task("copy-dist", function(){
